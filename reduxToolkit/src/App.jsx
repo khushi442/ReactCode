1f65ca3c64ae4b74f0f1b520b8ba24e0bd265574
Khushi Bharti <your_email@example.com>
2026-07-19 15:16:49 +0530

    reduxToolkit

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+  <>
+  <h1>Learn about Redux-Toolkit</h1>
+  <AddTodo/>
+  <Todos/>
+</>
+  )
+}
+export default App
